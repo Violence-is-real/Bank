c71b5a101c8b2c574ede4b0e598cab2a298db4e6
Калачев Я. К. <trevel37.fifa@gmail.com>
2026-03-15 17:42:16 +0300

    Были добавлены и проверены все необходимые тесты

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+
+            BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+
+            BankAccount account = new BankAccount("Mr. Roman Abramovich", beginningBalance);
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+        }
+
+        // ТЕСТЫ ДЛЯ CREDIT
+
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            double beginningBalance = 10.0;
+            double creditAmount = 5.0;
+            double expected = 15.0;
+
+            BankAccount account = new BankAccount("Test User", beginningBalance);
+
+            account.Credit(creditAmount);
+
+            double actual = account.Balance;
+
+            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+        }
+
+        [TestMethod]
+        public void Credit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            double beginningBalance = 10.0;
+            double creditAmount = -5.0;
+
+            BankAccount account = new BankAccount("Test User", beginningBalance);
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Credit(creditAmount));
+        }
+    }
+}
